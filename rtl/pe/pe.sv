2fe2e9999360a52e54d67c9a5bbec8dbb5b463a8
mani <mani@Mac.lan>
2026-07-26 15:30:27 -0900

    rtl: added register enable pins

--- rtl/pe/pe.sv
+++ rtl/pe/pe.sv
@@ -7,9 +7,9 @@ module pe #(
     input logic [0:0] clk_i,
     input logic [0:0] rstn_i,
     input logic [0:0] load_i,
-    input logic [0:0] advance_i,
-    input logic [0:0] valid_i,
-    output logic [0:0] valid_o,
+    input logic [0:0] input_en_i,
+    input logic [0:0] mul_en_i,
+    input logic [0:0] add_en_i,
     // input stream
     input logic signed [WIDTH_P-1:0] a_i,
     input logic signed [(2*WIDTH_P)+2:0] b_i,
@@ -25,13 +25,12 @@ module pe #(
     logic signed [(2*WIDTH_P)-1:0] product_w;
     logic signed [(2*WIDTH_P)-1:0] product_q;
     logic signed [(2*WIDTH_P)+2:0] sum_w;
-    logic [0:0] valid_q;
 
     always_comb begin
         // mult
-        product_w = a_i * weight_q;
+        product_w = a_q * weight_q;
         //add
-        sum_w = {{3{product_q[(2*WIDTH_P)-1]}}, product_q} + b_q;
+        sum_w = $signed({{3{product_q[(2*WIDTH_P)-1]}}, product_q}) + b_q;
     end
 
     // load
@@ -49,14 +48,13 @@ module pe #(
             a_q <= '0;
             b_q <= '0;
             product_q <= '0;
-            valid_q <= '0;
-        end else if (advance_i) begin
-            valid_q <= valid_i && !load_i;
-            if (valid_i && !load_i) begin
-                // acc = (streamed operand * local weight) + streamed partial
-                // align partial sum delay
+        end else begin
+            if (input_en_i) begin
                 a_q <= a_i;
                 b_q <= b_i;
+            end
+
+            if (mul_en_i) begin
                 product_q <= product_w;
             end
         end
@@ -67,13 +65,9 @@ module pe #(
         if (!rstn_i) begin
             c_o <= '0;
             d_o <= '0;
-            valid_o <= '0;
-        end else if (advance_i) begin
-            valid_o <= valid_q;
-            if (valid_q) begin
-                c_o <= a_q;
-                d_o <= sum_w;
-            end
+        end else if (add_en_i) begin
+            c_o <= a_q;
+            d_o <= sum_w;
         end
     end
 
